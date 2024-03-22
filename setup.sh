061845811a12fb19ce8eaa5a628df553a83d56cb
xenlor <152806260+xenlor@users.noreply.github.com>
2024-03-22 17:21:01 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 # Obtiene la ruta del directorio que contiene este script.
 rutaScript=$(dirname "${BASH_SOURCE[0]}")
-
+sudo curl -fsSL https://get.docker.com/ -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker ${USER}
 sudo docker compose -f $rutaScript/docker/cloudflare/docker-compose.yml up -d
 sudo docker compose -f $rutaScript/docker/cloudflare/docker-compose.yml up -d
 sudo docker compose -f $rutaScript/docker/duplicati/docker-compose.yml up -d
