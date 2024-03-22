4e497fca4e57e22788411b8105e1f8fd7d14d63a
xenlor <152806260+xenlor@users.noreply.github.com>
2024-03-22 17:15:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 # Obtiene la ruta del directorio que contiene este script.
 rutaScript=$(dirname "${BASH_SOURCE[0]}")
 
-docker-compose -f $rutaScript/docker/cloudflare/docker-compose.yml up -d
-docker-compose -f $rutaScript/docker/cloudflare/docker-compose.yml up -d
-docker-compose -f $rutaScript/docker/duplicati/docker-compose.yml up -d
-docker-compose -f $rutaScript/docker/filebrowser/docker-compose.yml up -d
-docker-compose -f $rutaScript/docker/heimdall/docker-compose.yml up -d
-docker-compose -f $rutaScript/docker/monitoring/docker-compose.yml up -d
+sudo docker compose -f $rutaScript/docker/cloudflare/docker-compose.yml up -d
+sudo docker compose -f $rutaScript/docker/cloudflare/docker-compose.yml up -d
+sudo docker compose -f $rutaScript/docker/duplicati/docker-compose.yml up -d
+sudo docker compose -f $rutaScript/docker/filebrowser/docker-compose.yml up -d
+sudo docker compose -f $rutaScript/docker/heimdall/docker-compose.yml up -d
+sudo docker compose -f $rutaScript/docker/monitoring/docker-compose.yml up -d
 
