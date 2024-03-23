a5790bfef501431ca7c576138ca045b8012770ab
xenlor <152806260+xenlor@users.noreply.github.com>
2024-03-23 13:04:52 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ else
 fi
 
 # Instalación de Portainer
-if [ ! "$(sudo docker ps -f name=portainer)" ]; then
+if [ ! "$(sudo docker ps -a | grep portainer)" ]; then
     echo "Instalando Portainer..."
     sudo docker volume create portainer_data
     sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
