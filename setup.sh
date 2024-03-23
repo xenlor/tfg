430455de4b06d0f1000bd5875e05315140a0c821
xenlor <152806260+xenlor@users.noreply.github.com>
2024-03-23 13:40:23 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,17 +9,22 @@ sudo chmod -R 777 $rutaScript
 echo "Actualizando repertorios..."
 sudo apt-get update -y > /dev/null
 sudo apt-get upgrade -y > /dev/null
+echo "Repertorios actualizados correctamente."
 
 # Instalar Fail2Ban
+echo "Instalando Fail2Ban..."
 sudo apt install fail2ban -y > /dev/null
+echo "Fail2Ban instalado correctamente."
 
 # Comprobar si Docker ya está instalado
 if ! command -v docker &> /dev/null; then
     echo "Docker no está instalado. Instalando Docker..."
     # Instalación de Docker
-    curl -sSL https://get.docker.com | sh
+    curl -sSL https://get.docker.com | sh  > /dev/null
     # Añade el usuario actual al grupo docker
-    sudo usermod -aG docker "$USER"
+    echo "Usuario '"$USER"' añadido al grupo 'docker'."
+    sudo usermod -aG docker "$USER"  > /dev/null
+    echo "Docker instalado correctamente."
 else
     echo "Docker ya está instalado."
 fi
@@ -27,8 +32,9 @@ fi
 # Instalación de Portainer
 if [ ! "$(sudo docker ps -a | grep portainer)" ]; then
     echo "Instalando Portainer..."
-    sudo docker volume create portainer_data
-    sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+    sudo docker volume create portainer_data > /dev/null
+    sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest  > /dev/null
+    echo "Portainer instalado correctamente."
 else
     echo "Portainer ya está instalado."
 fi
